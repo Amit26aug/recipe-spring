8cf8cfd65415be158cfa376ff53547eab814e7ab
amit.sharma@hitachivantara.com <alliswell420idiots>
2020-08-04 02:02:02 +0530

    Added OneToOne mapping for Notes and Recipe

--- src/main/java/com/august/recipe/controllers/IndexController.java
+++ src/main/java/com/august/recipe/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.august.recipe.controller;
+package com.august.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
